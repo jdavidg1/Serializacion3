694945b6c2d59a367aeb20e5d23dec9c4d051a0d
jdavidg1 <jdavidg_1@hotmail.com>
2019-12-28 21:06:22 -0500

    Creacion de la clase y el constructor

---
+++ Serializacion3/CAuto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacion3
+{
+    [Serializable]
+    class CAuto
+    {
+        //Declaración de atributos
+        private string modelo;
+        private double precio;
+
+        //Declaración del constructor
+
+        public CAuto (string dModelo, double dPrecio)
+        {
+            modelo = dModelo;
+            precio = dPrecio;
+        }
+
+
+
+
+    }
+}
